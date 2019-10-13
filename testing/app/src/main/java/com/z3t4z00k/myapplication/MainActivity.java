611aa4bfcc5300c2e3754e462cb20cb87339dab8
Piyusinhacom <56498260+Piyusinhacom@users.noreply.github.com>
2019-10-13 11:20:26 +0530

    Add files via upload

---
+++ testing/app/src/main/java/com/z3t4z00k/myapplication/MainActivity.java
@@ -0,0 +1,21 @@
+package com.z3t4z00k.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        System.out.println("hiee");
+        System.out.println("hiee");
+        System.out.println("Yo focker");
+        //yolo fock
+        System.out.println("hehe");
+        System.out.println("hey bro what's up ");
+
+    }
+}
